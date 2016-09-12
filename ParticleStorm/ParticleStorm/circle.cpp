a583ffc4379102476789d338d786b6f38b295602
Sandor Daemen <s_daemen@hotmail.nl>
2016-09-12 09:32:11 +0200

    ParticleStorm Test
    This is purely a test

---
+++ ParticleStorm/ParticleStorm/circle.cpp
@@ -0,0 +1,7 @@
+#include "circle.h"
+
+
+
+circle::circle()
+{
+}
---
+++ ParticleStorm/ParticleStorm/circle.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ofMain.h"
+class circle
+{
+public:
+	circle();
+
+	void createCircle(float size, COLOR16 color, float destructTime);
+	void moveCircle(float speed);
+
+};
+
